30e840d76d026f9f29a4cb76ebf3c097fe39beda
Jeremy <jerkku_astrom@hotmail.com>
2022-09-19 21:38:35 +0300

    tallennus

--- src/main/java/com/example/Bookstore/domain/Book.java
+++ src/main/java/com/example/Bookstore/domain/Book.java
@@ -6,8 +6,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 
 @Entity
 public class Book {
