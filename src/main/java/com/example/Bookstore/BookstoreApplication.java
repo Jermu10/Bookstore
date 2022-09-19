fee151de47c8931308674b464243e263b8062cb1
Jeremy <jerkku_astrom@hotmail.com>
2022-09-19 21:33:39 +0300

    lisätty database

--- src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -1,13 +1,31 @@
 package com.example.Bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.Bookstore.domain.Book;
+import com.example.Bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner demo (BookRepository repository) {
+		return (args) -> {
+			Book b1 = new Book("Testi", "Testinen", 2010, "tt-11-tt-11", 20.5);
+			Book b2 = new Book("Yritys", "Kakkonen", 2010, "yy-22-yy-22", 25.5);
+			Book b3 = new Book("Kokeilu", "Kolmonen", 2010, "kk-33-kk-33", 30.5);
+			
+			repository.save(b1);
+			repository.save(b2);
+			repository.save(b3);
+		};
 	}
 
 }
--- src/main/java/com/example/Bookstore/domain/Book.java
+++ src/main/java/com/example/Bookstore/domain/Book.java
@@ -1,12 +1,47 @@
 package com.example.Bookstore.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+
+@Entity
 public class Book {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
 	private String title;
 	private String author;
+	@Column(name="publishing_year")
 	private int year;
 	private String isbn;
 	private double price;
 	
+	
+	public Book() {}
+
+	public Book(String title, String author, int year, String isbn, double price) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.isbn = isbn;
+		this.price = price;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	
 	public String getTitle() {		
 		return title;
@@ -38,4 +73,6 @@ public class Book {
 	public void setPrice(double price) {
 		this.price = price;
 	}
+	
+
 }
