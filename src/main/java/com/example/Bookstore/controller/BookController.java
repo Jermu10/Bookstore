afb00f10404e8fb428590c8993f39d8da0240498
Jeremy <jerkku_astrom@hotmail.com>
2022-09-20 14:30:57 +0300

    Lisätty edit

--- src/main/java/com/example/Bookstore/controller/BookController.java
+++ src/main/java/com/example/Bookstore/controller/BookController.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.Bookstore.domain.Book;
 import com.example.Bookstore.domain.BookRepository;
 
 @Controller
@@ -20,9 +22,29 @@ public class BookController {
 		return "booklist";
 	}
 	
+	@GetMapping(value = "/add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+	
+	@PostMapping(value = "/save")
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
+	}
+	
 	@GetMapping(value = "/delete/{id}")
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 		repository.deleteById(bookId);
 		return "redirect:../booklist";
 	}
+	
+	@GetMapping(value = "/edit/{id}")
+	public String editBook(@PathVariable("id") Long bookId, Model model) {
+		model.addAttribute("book", repository.findById(bookId));
+		return "editbook";
+	}
+	
+	
 }
